6d79fa9e8547c445cd150913b143d86f41df5e9f
AzamovSiyovush <siyovush.azamov06@gmail.com>
2025-11-14 19:58:56 +0500

    test

--- RentalCar.Infrastructure/obj/Release/net9.0/RentalCar.Infrastructure.AssemblyInfo.cs
+++ RentalCar.Infrastructure/obj/Release/net9.0/RentalCar.Infrastructure.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("RentalCar.Infrastructure")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdd18832e5a0d67e3cc75ec5eec4fbcc422a7c39")]
 [assembly: System.Reflection.AssemblyProductAttribute("RentalCar.Infrastructure")]
